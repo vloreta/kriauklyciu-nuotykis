ff76d9235bec459d477f6b97c263668f0c3fc7d8
vloreta <ikt.loreta@gmail.com>
2026-07-07 19:01:42 +0200

    Configure Supabase cloud sync

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 window.KRIAUKLYCIU_CLOUD_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://gpfktbqeylspemolasfp.supabase.co",
+  supabaseAnonKey: "sb_publishable_a2TMYBxpxRUuOCDYNhbPTA_lEM3km-d",
   tableName: "family_data",
   familyKey: "kriauklyciu-nuotykis-main-family"
 };
